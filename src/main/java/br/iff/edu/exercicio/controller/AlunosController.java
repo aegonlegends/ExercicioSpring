297c10e957095242e23e313bd79cc5914c6805c7
Wesley Gomes <aegonlegends@gmail.com>
2018-06-05 17:49:54 -0300

    Adicionadas funções de editar e excluir

--- src/main/java/br/iff/edu/exercicio/controller/AlunosController.java
+++ src/main/java/br/iff/edu/exercicio/controller/AlunosController.java
@@ -1,8 +1,12 @@
 package br.iff.edu.exercicio.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,4 +35,19 @@ public class AlunosController {
 		return "redirect:/alunos";
 	}
 	
+	@RequestMapping(value="/excluir/{id}")
+	public String excluir(@PathVariable Long id, HttpServletRequest request, HttpServletResponse response){
+		
+	this.alunos.deleteById(id);
+	return "redirect:/alunos";
+	}
+	
+	@RequestMapping(value="/alterar/{id}")
+	public ModelAndView alterar(@PathVariable Long id, HttpServletRequest request, HttpServletResponse response){
+	ModelAndView mv = new ModelAndView("ListaAlunos");
+	mv.addObject("alunos", alunos.findAll());
+	Aluno aluno = alunos.findById(id).get();
+	mv.addObject(aluno);
+	return	mv;
+	}
 }
